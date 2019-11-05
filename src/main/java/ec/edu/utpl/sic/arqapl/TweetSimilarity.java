55192117a228364dd6336575548b2632c5ca95d6
xaviQuevedo <xavi_quevedo@hotmail.com>
2019-11-04 20:20:29 -0500

    Actulizacion TweetSimilarity
    Añadiendo 2 y 3 algoritmo a la clase TweetSimilarity.java

--- src/main/java/ec/edu/utpl/sic/arqapl/TweetSimilarity.java
+++ src/main/java/ec/edu/utpl/sic/arqapl/TweetSimilarity.java
@@ -1,21 +1,37 @@
 package ec.edu.utpl.sic.arqapl;
 
 
+import ec.edu.utpl.sic.arqapl.metrics.Cosine;
 import ec.edu.utpl.sic.arqapl.metrics.Jaccard;
+import ec.edu.utpl.sic.arqapl.metrics.Jaro;
 import ec.edu.utpl.sic.arqapl.metrics.Similaritable;
 
 public class TweetSimilarity {
     private Similaritable algorithm;
+    private Similaritable algorithm2;
+    private Similaritable algorithm3;
+
 
     public TweetSimilarity() {
         algorithm = new Jaccard();
+        algorithm2= new Cosine();
+        algorithm3= new Jaro();
     }
 
     public TweetSimilarity(Similaritable algorithm) {
         this.algorithm = algorithm;
     }
 
-    public double similarity(String tweet1, String tweet2) {
+    public double similarityJaccard(String tweet1, String tweet2) {
         return algorithm.similarity(tweet1, tweet2);
     }
+    public double similarityCosine(String tweet1, String tweet2) {
+        return algorithm2.similarity(tweet1, tweet2);
+    }
+    public double similarityJaro(String tweet1,String tweet2){return algorithm3.similarity(tweet1,tweet2);
+    }
+    /*@Override
+    public String toString (){
+        return Similaritable;
+    }*/
 }
